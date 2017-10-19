b218223dfb47b74e10e7256a372aa4f568fe660d
Alex Sikora <zaxxle@gmail.com>
2017-10-19 14:25:37 -0400

    Initial commit of project

---
+++ ReDoDay/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ReDoDay
+//
+//  Created by Alex Sikora on 10/19/17.
+//  Copyright © 2017 Alex Sikora. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
